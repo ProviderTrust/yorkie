ee8af644f6539e0dc9b7562ca1e11983bcad8012
Stephen Miller <smiller@providertrust.com>
2020-08-12 15:50:42 -0500

    VP-2466 Upgrading yorkie to work with execa v2. Is a v3 or v4 upgrade in the wings? Who knows???

--- src/runner.js
+++ src/runner.js
@@ -4,26 +4,13 @@ const execa = require('execa')
 
 const cwd = process.cwd()
 const pkg = fs.readFileSync(path.join(cwd, 'package.json'))
-//const pkg = fs.readFileSync(path.join('/Users/smiller/repos/vendorproof-ui', 'package.json'))
-
-console.log('---------')
-console.log(JSON.parse(pkg))
-console.log('---------')
 
 const hooks = JSON.parse(pkg).gitHooks
 if (!hooks) {
   process.exit(0)
 }
 
-console.log('---------')
-console.log(hooks)
-console.log('---------')
-
 const hook = process.argv[2]
-
-console.log('---------')
-console.log(hook)
-console.log('---------')
 
 const command = hooks[hook]
 if (!command) {
@@ -34,11 +21,9 @@ console.log(` > running ${hook} hook: ${command}`)
 try {
   var pid = execa(command, [],  { stdio: 'inherit', shell : true })
   pid.catch(e => { 
-    console.log('a problem')
-    console.log(e)
+    console.error(e)
     process.exit(1) 
   })
 } catch (e) {
-  console.log('new problem')
   process.exit(1)
 }
